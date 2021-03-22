d406b2113f6f9470cf7b00d1cfab7697866bc9be
keduma <kevin.dupont-marin@outlook.fr>
2021-03-22 15:50:22 +0100

    Creation table avec relation

--- mysql/creation.sql
+++ mysql/creation.sql
@@ -1,8 +1,18 @@
+CREATE TABLE Address
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    street VARCHAR(255),
+    city VARCHAR(255),
+    country VARCHAR(255),
+    longitude DOUBLE,
+    latitude DOUBLE
+)ENGINE=InnoDB;
+
 CREATE TABLE Members
 (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     firstname VARCHAR(255),
-    name VARCHAR(255),
+    lastname VARCHAR(255),
     mail VARCHAR(255),
     pseudo VARCHAR(255),
     password VARCHAR(255)
@@ -14,10 +24,8 @@ CREATE TABLE Events
     title VARCHAR(255),
     description VARCHAR(255),
     date VARCHAR(255),
-    longitude DOUBLE,
-    latitude DOUBLE,
-    adress VARCHAR(255),
     url VARCHAR(255),
+    id_address INT,
     id_creator INT,
     id_message INT
 )ENGINE=InnoDB;
@@ -36,3 +44,18 @@ CREATE TABLE Messages
     date VARCHAR(255),
     id_member INT
 )ENGINE=InnoDB;
+
+ALTER TABLE Events
+ADD CONSTRAINT fk_event_creator FOREIGN KEY (id_creator) REFERENCES Members(id);
+ALTER TABLE Events
+ADD CONSTRAINT fk_event_message FOREIGN KEY (id_message) REFERENCES Messages(id);
+ALTER TABLE Events
+ADD CONSTRAINT fk_event_address FOREIGN KEY (id_address) REFERENCES Address(id);
+
+ALTER TABLE Guests
+ADD CONSTRAINT fk_guest_event FOREIGN KEY (id_event) REFERENCES Events(id);
+ALTER TABLE Guests
+ADD CONSTRAINT fk_guest_member FOREIGN KEY (id_member) REFERENCES Members(id);
+
+ALTER TABLE Messages
+ADD CONSTRAINT fk_message_member FOREIGN KEY (id_member) REFERENCES Members(id);

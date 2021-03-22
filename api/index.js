94a48edf02415a7a4a079d53e3a1243f012be5a5
GaetanCom <comacle.gaetan@gmail.com>
2021-03-22 17:04:02 +0100

    test concluant

--- api/index.js
+++ api/index.js
@@ -1,1 +1,28 @@
-console.log("Test")
+const express = require('express');
+const app = express();
+const NodeGeocoder = require('node-geocoder');
+
+const options = {
+    provider: 'mapquest',
+    apiKey: 'SIrr3b2xAP9dzPkiOh6vFNr6PptSoCdk',
+    formatter: null
+}
+
+const geocoder = NodeGeocoder(options);
+
+app.get('/', async (req, res, next) => {
+    const dataLoc = await geocoder.geocode({
+        address: '9 rue de mon desert',
+        country: 'France',
+        zipcode: '54000'
+    });
+
+    console.log(dataLoc);
+
+    res.send("I'm under water");
+});
+
+
+app.listen(3000, () => {
+    console.log(`Example app listening on port 3000!`)
+});

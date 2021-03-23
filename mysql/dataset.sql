b4aaa9751a9a0086799af245d23958d5cf37e982
keduma <kevin.dupont-marin@outlook.fr>
2021-03-23 10:56:23 +0100

    dataset members

---
+++ mysql/dataset.sql
@@ -0,0 +1,8 @@
+INSERT INTO Members (id, pseudo, password, firstname, lastname, mail) VALUES
+(1, 'Admin', 'Admin123', 'Admin', 'Admin', 'Admin@gmail.com'),
+(2, 'Keduma',  'Keduma123', 'Kevin', 'Dupont-Marin', 'Keduma@gmail.com'),
+(3, 'Guitar',  'Guitar123', 'Gaetan', 'Comacle', 'Guitar@gmail.com'),
+(4, 'Zacpot',  'Zacpot123', 'Arthur', 'Loeuillet',  'Zacpot@gmail.com'),
+(5, 'Trema',  'Trema123', 'Loic', 'Oberle', 'Trema@gmail.com'),
+(6, 'Shiryu',  'Shiryu123', 'Scott', 'Halgue', 'Shiryu@gmail.com'),
+(7, 'Harry',  'Harry123', 'Harry', 'Potter', 'Harry@gmail.com');

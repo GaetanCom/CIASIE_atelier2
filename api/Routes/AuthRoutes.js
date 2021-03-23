f367a0f50ab92d82dc98258b7a1f8982b9d610e0
GaetanCom <comacle.gaetan@gmail.com>
2021-03-23 18:42:50 +0100

    Correction Auth

--- api/Routes/AuthRoutes.js
+++ api/Routes/AuthRoutes.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require("body-parser");
 
 //router.use(bodyParser.urlencoded({ extended: false }));
 //router.use(bodyParser.json());
 
 const bdd = require("../utils/DBclients");
+const { json } = require('body-parser');
 
-router.get("/", (req,res)=>{
-    bdd.query('SELECT * FROM Members', function(err, rows, fields) {
-        if (err) throw err;
-        let data=[]
-        for (let i = 0; i < rows.length; i++){
+router.get("/", async (req, res, next)=>{
+
+    try {
+        const allUsers = await bdd.query('SELECT * FROM Members');
+        let data = [];
+
+        console.log(allUsers);
+
+        allUsers.forEach(element => {
             data.push({
-                "id": rows[i].id,
-                "firstname": rows[i].firstname,
-                "lastname": rows[i].lastname,
-                "mail": rows[i].mail,
-                "pseudo": rows[i].pseudo,
-                "password":rows[i].password
+                "id": element.id,
+                "firstname": element.firstname,
+                "lastname": element.lastname,
+                "mail": element.mail,
+                "pseudo": element.pseudo,
+                "password":element.password
             })
-        }
-    res.send(data)   
-  //console.log('The solution is: ', rows[0].solution);
-});
+        });
+        res.json(data);
+    } catch(err) {
+        console.error(err);
+        throw new Error(err);
+    }
 })
 
 router.get('/:id', (req, res) => {
@@ -46,116 +54,113 @@ router.get('/:id', (req, res) => {
     })
 });
 
-router.post("/connection/signup", (req,res) => {
-    console.log(req.body);
-    let firstname = req.body.firstname
-    let lastname = req.body.lastname
-    let mail = req.body.mail
-    let pseudo = req.body.pseudo
-    let password=req.body.password
-     var sql = "INSERT INTO Members (firstname,lastname,mail,pseudo,password) VALUES (?,?,?,?,?)";
-    bdd.query(sql, [firstname,lastname,mail,pseudo,password],function (err, result) {
+// router.post("/connection/signup", (req,res) => {
+//     console.log(req.body);
+//     let firstname = req.body.firstname
+//     let lastname = req.body.lastname
+//     let mail = req.body.mail
+//     let pseudo = req.body.pseudo
+//     let password=req.body.password
+//      var sql = "INSERT INTO Members (firstname,lastname,mail,pseudo,password) VALUES (?,?,?,?,?)";
+//     bdd.query(sql, [firstname,lastname,mail,pseudo,password],function (err, result) {
 
-        if (err) {
-            throw err;
-            res.json(false)
-    }
-        console.log("1 record inserted");
-          res.json(true)
-  });
+//         if (err) {
+//             throw err;
+//             res.json(false)
+//     }
+//         console.log("1 record inserted");
+//           res.json(true)
+//   });
   
     
-})
+// })
 
-router.get('/connection/checkRegistered', (req, res) => {
-    let login = req.query.login
-    let pwd = req.query.pwd
-   bdd.query('SELECT * FROM Members WHERE pseudo=? AND password=?', [login,pwd], function (err, rows, fields) {
-        if (err) throw err;
-       if (!rows.length == 0) {
-           res.json({
-               "id": rows[0].id,
-               "firstname": rows[0].firstname,
-               "lastname": rows[0].lastname,
-               "mail": rows[0].mail,
-               "pseudo": rows[0].pseudo,
-               "password": rows[0].password
-           })
-       } else {
-           res.json(-1)
-        }
+// router.get('/connection/checkRegistered', (req, res) => {
+//     let login = req.query.login
+//     let pwd = req.query.pwd
+//    bdd.query('SELECT * FROM Members WHERE pseudo=? AND password=?', [login,pwd], function (err, rows, fields) {
+//         if (err) throw err;
+//        if (!rows.length == 0) {
+//            res.json({
+//                "id": rows[0].id,
+//                "firstname": rows[0].firstname,
+//                "lastname": rows[0].lastname,
+//                "mail": rows[0].mail,
+//                "pseudo": rows[0].pseudo,
+//                "password": rows[0].password
+//            })
+//        } else {
+//            res.json(-1)
+//         }
        
   
-    })
-return 	
-});
+//     })
+// return 	
+// });
 
-router.post("/update/firstname", (req, res)=> {
-    let newFirstName = req.body.firstname
-    let id = req.body.id
-    bdd.query("UPDATE Members SET firstname=? WHERE id=?", [newFirstName, id], function (err, row, fields) {
-        if (err) {
-              throw err;
-              res.json(false)
-          } 
+// router.post("/update/firstname", (req, res)=> {
+//     let newFirstName = req.body.firstname
+//     let id = req.body.id
+//     bdd.query("UPDATE Members SET firstname=? WHERE id=?", [newFirstName, id], function (err, row, fields) {
+//         if (err) {
+//               throw err;
+//               res.json(false)
+//           } 
 
-       res.json(true)
-    })
-})
+//        res.json(true)
+//     })
+// })
 
-router.post("/update/lastname", (req, res)=> {
-    let newLastname = req.body.lastname
-    let id = req.body.id
-    bdd.query("UPDATE Members SET lastname=? WHERE id=?", [newLastname, id], function (err, row, fields) {
-        if (err) {
-              throw err;
-              res.json(false)
-          } 
+// router.post("/update/lastname", (req, res)=> {
+//     let newLastname = req.body.lastname
+//     let id = req.body.id
+//     bdd.query("UPDATE Members SET lastname=? WHERE id=?", [newLastname, id], function (err, row, fields) {
+//         if (err) {
+//               throw err;
+//               res.json(false)
+//           } 
 
-       res.json(true)
-    })
-})
+//        res.json(true)
+//     })
+// })
 
-router.post("/update/mail", (req, res)=> {
-    let newMail = req.body.mail
-    let id = req.body.id
-    bdd.query("UPDATE Members SET mail=? WHERE id=?", [newMail, id], function (err, row, fields) {
-        if (err) {
-              throw err;
-              res.json(false)
-          } 
+// router.post("/update/mail", (req, res)=> {
+//     let newMail = req.body.mail
+//     let id = req.body.id
+//     bdd.query("UPDATE Members SET mail=? WHERE id=?", [newMail, id], function (err, row, fields) {
+//         if (err) {
+//               throw err;
+//               res.json(false)
+//           } 
 
-       res.json(true)
-    })
-})
+//        res.json(true)
+//     })
+// })
 
-router.post("/update/pseudo", (req, res)=> {
-    let newPseudo = req.body.pseudo
-    let id = req.body.id
-    bdd.query("UPDATE Members SET pseudo=? WHERE id=?", [newPseudo, id], function (err, row, fields) {
-        if (err) {
-              throw err;
-              res.json(false)
-          } 
+// router.post("/update/pseudo", (req, res)=> {
+//     let newPseudo = req.body.pseudo
+//     let id = req.body.id
+//     bdd.query("UPDATE Members SET pseudo=? WHERE id=?", [newPseudo, id], function (err, row, fields) {
+//         if (err) {
+//               throw err;
+//               res.json(false)
+//           } 
 
-       res.json(true)
-    })
-})
+//        res.json(true)
+//     })
+// })
 
-router.post("/update/password", (req, res)=> {
-    let newPassword = req.body.password
-    let id = req.body.id
-    bdd.query("UPDATE Members SET password=? WHERE id=?", [newPassword, id], function (err, row, fields) {
-        if (err) {
-              throw err;
-              res.json(false)
-          } 
+// router.post("/update/password", (req, res)=> {
+//     let newPassword = req.body.password
+//     let id = req.body.id
+//     bdd.query("UPDATE Members SET password=? WHERE id=?", [newPassword, id], function (err, row, fields) {
+//         if (err) {
+//               throw err;
+//               res.json(false)
+//           } 
 
-       res.json(true)
-    })
-})
-
-
-
+//        res.json(true)
+//     })
+// })
 
 module.exports = router;

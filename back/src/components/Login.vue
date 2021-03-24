11a8ee7157a548e18361957cfd12e7fef8c45f67
Jean-Baptiste Benard <jean-baptiste.benard5@etu.univ-lorraine.fr>
2021-03-24 21:58:11 +0100

    connexion admin + lister users

---
+++ back/src/components/Login.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="login">
+
+        <section class="error" v-if="errored">
+            <b-alert show variant="danger"><i>Nous sommes désolés, {{ alertError }}. Veuillez réessayer.</i></b-alert>
+        </section>
+
+        <h1>Veuillez vous connecter</h1>
+        <input type="text" name="username" v-model="input.username" placeholder="Login" />
+        <input type="password" name="password" v-model="input.password" placeholder="Password" />
+
+        <b-button class="btn" variant="light" v-on:click="login()">Connexion</b-button>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                },
+                loading:false,
+                errored:false,
+                alertError:"",
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "" && this.input.username.includes("Admin")) {
+                    const url = "http://localhost:19080/users/connection/checkRegistered?login="+this.input.username+"&pwd="+this.input.password;
+                    axios
+                        .get(url)
+                        .then(response => {
+                            if(response.data == -1){
+                                console.log(response);
+                                this.errored = true;
+                                this.alertError="Votre Mot de Passe est incorrect"
+                            }else{
+                                this.$emit("authenticated", true);
+                                this.$router.replace({ name: "home" });
+                            }
+                        })
+                        .catch(error => {
+                            console.log(error)
+                            this.errored = true
+                        })
+                        .finally(() => this.loading = false)
+
+                }else{
+                    this.errored=true;
+                    this.alertError = "Votre Login ou Mot de Passe est incorrect";
+                    console.log("error");
+                }
+            }
+            }
+    }
+</script>
+
+<style scoped>
+    #login {
+        width: 500px;
+        background-color: #FFFFFF;
+        margin: auto;
+        padding: 20px;
+    }
+
+    .btn{
+        margin-top: 20px;
+        color: deepskyblue;
+
+    }
+
+
+
+</style>
---
+++ back/src/components/Users.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="pannel">
+    <h1>Utilisateurs</h1>
+    <p>
+      Retrouvez ici tous les utilisateurs qui utilisent Reunionou
+    </p>
+
+    <b-list-group class="liste">
+      <b-list-group-item class="d-flex justify-content-between align-items-center" v-for="user in users">
+        <b-badge variant="dark" pill>{{user.id}}</b-badge>
+        <div><i>Nom: </i>
+          {{user.firstname}}
+        {{user.lastname}}
+        </div>
+        <div><i>Mail: </i> {{user.mail}}</div>
+
+        <b-button variant="outline-danger">Supprimer</b-button>
+      </b-list-group-item>
+
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'users',
+    data() {
+      return {
+        users:[]
+      };
+    },
+    mounted() {
+      axios
+              .get("http://localhost:19080/users")
+              .then (response => {
+                console.log(response.data)
+                this.users = response.data
+      })
+    }
+  }
+</script>
+
+<style>
+
+  .liste{
+    width: 50%;
+    margin: auto;
+  }
+
+</style>

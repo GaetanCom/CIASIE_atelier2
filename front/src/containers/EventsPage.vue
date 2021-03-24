500545166554abc3765586f167ea21e6c346495b
Zacpot <arthur.loeuillet@orange.fr>
2021-03-24 14:25:03 +0100

    ajout de EventsPage

---
+++ front/src/containers/EventsPage.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div>
+        <h1>Events Page</h1>
+
+        <div>
+            <a :href="'/create'">Créer un Évènement</a>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>

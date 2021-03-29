58014fa47387c8e515a25d9fa9619c3a6bfd3dba
GaetanCom <comacle.gaetan@gmail.com>
2021-03-29 14:52:09 +0200

    Menu mis à jour

--- front/src/components/navigation/Navigation.vue
+++ front/src/components/navigation/Navigation.vue
@@ -1,37 +1,96 @@
 <template>
 
-    <nav class="navigation">
-        <a :href="'/'">
-            <img 
-                src="../../assets/logo_reunionou.png" 
-                alt="Logo Reunionou" 
-                class="navigation_logo" />
-        </a>
-        <li>
-            <ul> <a :href="'/events'">Événements</a> </ul>
-            <ul> <a :href="'/register'">inscrptions</a> </ul>
-        </li>
-    </nav>
+    <!-- <div class="navigation">
+        <nav class="navigation_header">
+            <a :href="'/'">
+                
+            </a>
+            <MenuItems />
+        </nav>
+
+    </div> -->
+
+    <div class="navigation">
+        <b-navbar toggleable="lg" type="dark" class="navbar_style">
+            <b-navbar-brand href="/">
+                    <img 
+                        src="../../assets/logo_reunionou_white.png" 
+                        alt="Logo Reunionou" 
+                        class="navigation_header_logo" />
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto hamburger">
+                <b-navbar-nav>
+                    <!-- <b-nav-item :href="'/events'">Mes événements</b-nav-item> -->
+                    <b-nav-item :href="'/events'" >Mes évènements</b-nav-item>
+                    <b-nav-item :href="'/'" >Se connecter</b-nav-item>
+                    <b-nav-item :href="'/register'" >S'inscrire</b-nav-item>
+                </b-navbar-nav>
+
+
+                <b-nav-item-dropdown right>
+                    
+                <!-- Using 'button-content' slot -->
+                <template #button-content>
+                    <em>User</em>
+                </template>
+                <div class="userItem">
+                    <b-dropdown-item :href="'/user/'">Profil</b-dropdown-item>
+                    <b-dropdown-item :href="'/logout'">Se déconnecter</b-dropdown-item>
+
+                </div>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
     
 </template>
 
 <script>
+
 export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 
 .navigation {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #efefef;
+    margin-bottom: 10px;
 }
 
-.navigation_logo {
-    width: 300px;
-    height: 50px;
+.userItem {
+    display: flex;
+    justify-content: space-around;
 }
+
+.navigation_header_logo {
+    width: 200px;
+}
+
+.navbar_style {
+    background-color: #00bae1;
+    font-weight: bold;
+}
+
+@media (min-width: 992px) {
+
+    .userItem {
+        flex-direction: column;
+    }
+
+    .navigation_header_logo {
+        width: 300px;
+    }
+    
+}
+
+
 
 </style>
